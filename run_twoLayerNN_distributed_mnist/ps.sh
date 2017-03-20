288a9dec229f519297b0852ba27533a29374bc5a
kuozhang <kzhang28@buffalo.edu>
2017-03-20 16:48:55 -0400

    two layers neural network distributed Tensorflow example

---
+++ run_twoLayerNN_distributed_mnist/ps.sh
@@ -0,0 +1,6 @@
+/Users/kuozhang/anaconda/envs/tensorflow/bin/python  mnist_2hiddenLayerNN_distributed_ph.py \
+	--ps_hosts=localhost:2222 \
+	--worker_hosts=localhost:2224,localhost:2226 \
+	--job_name=ps \
+	--task_index=0 \
+	--num_workers=2
---
+++ run_twoLayerNN_distributed_mnist/worker1.sh
@@ -0,0 +1,6 @@
+/Users/kuozhang/anaconda/envs/tensorflow/bin/python  mnist_2hiddenLayerNN_distributed_ph.py \
+	--ps_hosts=localhost:2222 \
+	--worker_hosts=localhost:2224,localhost:2226 \
+	--job_name=worker \
+	--task_index=1 \
+	--num_workers=2
